575db675ec1751edf2863ec2be1da0f7f6b4672d
GMDeese <g.m.deese@gmail.com>
2020-09-09 17:25:40 -0700

    added dynamic dropdown functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,26 @@
-var task;
+var user;
 function captureInput(event) {
-  task = event.target.value;
-  console.log(task);
+  user = event.target.value;
+  console.log(user);
 }
 
-function addTask() {
+function addUser() {
   var newItem = document.createElement("li");
-  newItem.innerText = task;
+  newItem.innerText = user;
   document.getElementById("list").appendChild(newItem);
+  newItem.setAttribute("class", "option");
+  var dropdownList = document.getElementsByClassName("users");
+  for (var i = 0; i < dropdownList.length; i++) {
+    // Access each Dropdown list element
+    dropdownList[i];
+    // Create and options element
+    var createOption = document.createElement("option");
+    // update createOption with user list item
+    createOption.innerText = user;
+    // Append options element with dropdown element
+    dropdownList[i].appendChild(createOption);
+  }
 }
 
-document.getElementById("todo").addEventListener("change", captureInput);
-document.getElementById("add").addEventListener("click", addTask);
+document.getElementById("userInput").addEventListener("change", captureInput);
+document.getElementById("add").addEventListener("click", addUser);
