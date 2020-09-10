e7417b1aebd6f3100e139e14c20019755fa071d5
GMDeese <g.m.deese@gmail.com>
2020-09-09 18:45:59 -0700

    latest iteration

--- js/javascript.js
+++ js/javascript.js
@@ -20,9 +20,31 @@ function addUser() {
     // Append options element with dropdown element
     dropdownList[i].appendChild(createOption);
     var inputfield = document.getElementById("userInput");
-    document.getElementById("userInput").reset();
   }
 }
+
+
+funtion addstory() {
+  var newStory = document.createElement("li");
+  newItem.innertext = document
+  document.getElementById("stories").appendChild(newItem);
+  var storyList = document.getElementsByClassName("textarea1");
+  for (var i = 0; i < dropdownlist.length; i++) {
+  
+    // Access each story list element
+      storyList[i]
+    // Create Story Element
+      var createStory = document.createElement("li")
+    // update story with option dropdown value and textareas in the class
+      createStory.innerText = 
+    // Append story element with dropdown value and textareas in the class
+      storylist[i].appendChild(createStory);
+    var finalStory = document.getElementById("submitButton");
+  }
+  
+
+}
+//Create list element for each user story
 
 document.getElementById("userInput").addEventListener("change", captureInput);
 document.getElementById("add").addEventListener("click", addUser);
